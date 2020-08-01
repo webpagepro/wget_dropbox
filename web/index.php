d18499f70a254a9baf293726da0c4ccda2a356a3
webpagepro <webpagepro@aaol.com>
2020-08-01 10:33:25 -0700

    Refactored input for entire Dropbox URL

--- web/index.php
+++ web/index.php
@@ -13,17 +13,26 @@ error_reporting(-1);
 </head>
 
 <body>
-    <p>Workshop%3A%20Hyper-Realistic%20Multimedia%20for%20Enhanced%20Quality%20of%20Experience</p>
+    <p>&nbsp;</p>
+    <h3>Step One: Download Dropbox Video</h3>
     <form method="post" action="wget.php" name="getDropox">
-        <p>Enter video name:
-            <input type="text" name="name" value="">
-            <select name="extension">
+        <div>Enter Dropbox Link:</div>
+
+
+        <input type="text" name="download_url" value="" style="width:80%;height:60px;">
+        <!-- select name="extension">
                 <option>mp4</option>
                 <option>other</option>
-            </select>
-        </p>
-        <p><input type="submit" name="submit" value="Submit" /></p>
+            </!-->
+        </p:>
+        <p><input type="submit" name="submit" value="Download" /></p>
     </form>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <div>Example Dropbox Link:</div>
+    <p style="width:80%;">
+        https://www.dropbox.com/s/y2m1azgp1dptoqn/Workshop%3A%20Hyper-Realistic%20Multimedia%20for%20Enhanced%20Quality%20of%20Experience.mp4?dl=1
+    </p>
     <script></script>
 </body>
 
--- web/wget.php
+++ web/wget.php
@@ -1,28 +1,48 @@
 <?php
 
 
-$input_name = $_POST['name'];
-$post_form = $_POST['submit'];
-$extension = $_POST['extension'];
-$url = "https://www.dropbox.com/s/y2m1azgp1dptoqn/";
-$part1 = $input_name . "." . $extension . "?dl=1 -o " . $input_name . "." . $extension; 
+//"https://www.dropbox.com/s/y2m1azgp1dptoqn/Workshop%3A%20Hyper-Realistic%20Multimedia%20for%20Enhanced%20Quality%20of%20Experience.mp4?dl=1";  
 
-$front_clip = "fmpeg -i " . $input_name . ".$extension" . " -ss 0:20:00 -c:v copy -c:a copy" . $input_name . "-clipped" . ".$extension";
+$input_url = $_POST['download_url'];
+echo "****************";
+echo $input_url;
+echo "<br><br>";
+$dropbox_url = "https://www.dropbox.com/s/y2m1azgp1dptoqn/";
+$strip_suffix = str_replace("?dl=1", "", $input_url);
+$remove_prefix = str_replace($dropbox_url, "", $strip_suffix);
+$remove_mp4 = str_replace("mp4", "", $remove_prefix);
+$strip_space = str_replace("%3A%20", "-", $remove_prefix);
+$remove_colon = str_replace("%20", "_", $strip_space);
+echo "<br><br>******remove_colon*********";
 
-$rear_clip = "ffmpeg -i " . $input_name . "-clipped" . "." . $extension . " -ss 0:00:00 -to 0:00:30 -c:v copy -c:a copy " . $input_name . "-clipped" . $extension . " clipped-again" . $extension;
+echo $remove_colon;
+echo "<br><br>";
+
+$part1 = $input_url . " -O " . $remove_colon; 
+$clippings = array($strip_space, $remove_colon);
+
+//--------DOWNLOAD----------------
+exec("wget -q -O -c $part1");
+
+$remove_mp4 = str_replace("mp4", "", $remove_colon);
+echo "<br>REMOVE MP4<br>";
+echo $remove_mp4 . "<br><br>";
+$rename_mp4 = str_replace(".", "-clipped.mp4", $remove_mp4);
+$front_clip = "ffmpeg -i " . $remove_colon . " -ss 0:20:00 -c:v copy -c:a copy " . $rename_mp4;
+
+$rename_mp4_again = str_replace("-clipped.mp4", "-clipped-again.mp4", $rename_mp4);
+$rear_clip = "ffmpeg -i " . $rename_mp4 . " -ss 0:00:00 -to 0:00:30 -c:v copy -c:a copy " . $rename_mp4_again;
 
 $clippings = array($front_clip, $rear_clip);
 
-$final = $url . $part1; 
-exec("wget $final");
-//echo $final;
+
 echo "<br><br>";
 echo $front_clip;
+//exec("wget -q -O -c $front_clip");
+
 echo "<br><br>";
 echo $rear_clip;
 echo "<br><br>";
-
-
 
 ?>
 
@@ -30,16 +50,13 @@ echo "<br><br>";
 <title>POST</title>
 
 <head>
-    <script>
-
-
-    </script>
+    <script> </script>
 
 </head>
 
 <body>
     <p>Video that is downloading:</p>
-    <p><?php echo $final;  ?></p>
+    <p><?php echo $part1;  ?></p>
 </body>
 
 </html>
